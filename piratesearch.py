4f79a0f32d3e9ac2bc5532cd6dba872aa5dc03ad
kuco23 <nseverkar@gmail.com>
2019-07-04 02:09:53 +0200

    torrents listed with data

--- piratesearch.py
+++ piratesearch.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from microlib import safeget, log
 
 PIRATE_BAY = 'https://thepiratebay3.org'
-UNIT2NUM = {'MiB' : 1, 'GiB' : 10**3} # std unit is MiB
+UNIT2NUM = {'KiB' : 10**(-3), 'MiB' : 1, 'GiB' : 10**3} # std unit is MiB
 LOG_FILE = 'magnet.log'
 
 # set the terminal argument parser
@@ -22,20 +22,25 @@ url = PIRATE_BAY + '/index.php'
 query_page = safeget(url, params = {'q' : vals.tname})
 assert query_page, 'Pirate Bay Response Not Valid'
 
-# filter the data to find an acceptable torrent
-szrgx = re.compile(r'Size (?P<size>[\d\.]+) (?P<unit>GiB|MiB)')
+# filter the data to find an acceptable torrents (magnet links)
+magnets = []
+szrgx = re.compile(rf'Size (?P<size>[\d\.]+) (?P<unit>KiB|MiB|GiB)')
 soup = BeautifulSoup(query_page.content, 'html.parser')
 torrents = soup.select('#searchResult tr')
-torrents.pop(0)
 for torrent in torrents:
-    seeds, leachs = (int(x.text) for x in torrent.select('td[align=right]'))
-    specs = torrent.select_one('font.detDesc').text
-    specs = szrgx.search(normalize('NFKD', specs))
+    peers_data = torrent.select('td[align=right]')
+    specs = torrent.select_one('font.detDesc')
+    if not all((peers_data, specs)): continue
+    seeds, leachs = (int(x.text) for x in peers_data)
+    specs = szrgx.search(normalize('NFKD', specs.text))
     size = float(specs.group('size')) * UNIT2NUM[specs.group('unit')]
     if seeds >= vals.msd and leachs >= vals.mlc and \
        vals.msz <= size <= vals.xsz:
         magnet = torrent.select_one('td:nth-child(2) > a').attrs['href']
-        os.system('start ' + magnet)
-        if input('ok? y/n: ') == 'y':
-            log(magnet, LOG_FILE)
-            break
+        print(len(magnets), seeds, leachs, str(size) + ' MiB', sep=' - ')
+        magnets.append(magnet)
+
+# ask user for magnet link index
+idx = input('get magnet <index>: ')
+os.system('start ' + magnets[idx])
+log(magnets[idx], LOG_FILE)
