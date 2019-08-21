804f389ecf321316d7435df4b23bb96e85b62eb1
kuco23 <nseverkar@gmail.com>
2019-08-21 23:10:49 +0200

    rename files

---
+++ search_piratebay.py
@@ -0,0 +1,56 @@
+from math import inf
+from unicodedata import normalize
+import os, argparse, re
+from bs4 import BeautifulSoup
+from microlib import safeget, log
+
+TORRENT_TYPES = ('audio', 'video', 'applications', 'games', 'porn')
+UNIT2NUM = {'KiB' : 10**(-3), 'MiB' : 1, 'GiB' : 10**3} # std unit is MiB
+LOG_FILE = 'magnet.log'
+
+# set the terminal argument parser
+args = argparse.ArgumentParser(description='torrent specs')
+args.add_argument('tname', metavar='torrent_name', type=str)
+args.add_argument('-typ', metavar='torrent_type', type=str,
+                  nargs='+', choices=TORRENT_TYPES, default=[])
+args.add_argument('-msd', metavar='minseeds', type=int, default=0)
+args.add_argument('-mlc', metavar='minleachs', type=int, default=0)
+args.add_argument('-msz', metavar='minsize_MiB', type=int, default=0)
+args.add_argument('-xsz', metavar='maxsize_MiB', type=int, default=inf)
+vals = args.parse_args()
+
+# get the page containing torrents
+url = 'https://thepiratebay3.org/index.php'
+payload = {'q': vals.tname, **{typ: 'on' for typ in vals.typ}}
+query_page = safeget(url, params = payload)
+assert query_page, 'Pirate Bay Response Not Valid'
+
+# filter the data to find an acceptable torrents (magnet links)
+magnets = []
+szrgx = re.compile(r'Size (?P<size>[\d\.]+) (?P<unit>KiB|MiB|GiB)')
+soup = BeautifulSoup(query_page.content, 'html.parser')
+torrents = soup.select('#searchResult tr')
+for torrent in torrents:
+    peers = torrent.select('td[align=right]')
+    specs = torrent.select_one('font.detDesc')
+    if not all((peers, specs)): continue
+    seeds, leachs = (int(x.text) for x in peers)
+    specs = szrgx.search(normalize('NFKD', specs.text))
+    size = float(specs.group('size')) * UNIT2NUM[specs.group('unit')]
+    if seeds >= vals.msd and leachs >= vals.mlc and \
+       vals.msz <= size <= vals.xsz:
+        name = torrent.select_one('div.detName > a').text
+        magnet = torrent.select_one('td:nth-child(2) > a').attrs['href']
+        magnets.append((magnet, name, seeds, leachs, size))
+
+# print the available torrent data
+for i, data in enumerate(magnets):
+    try: print(f' \033[95m{i}\033[0m ', *data[1:], sep=' \033[92m||\033[0m ')
+    except Exception: continue
+
+# ask user for magnet link index
+try: idx = int(input('get magnet <index>: '))
+except ValueError: exit()
+if 0 <= idx < len(magnets):
+    os.system('start ' + magnets[idx][0])
+    log(magnets[idx][0], LOG_FILE)
---
+++ search_subtitles.py
@@ -0,0 +1,34 @@
+from io import BytesIO
+import os, argparse
+from zipfile import ZipFile
+from urllib.parse import urlsplit, parse_qs
+from bs4 import BeautifulSoup
+from microlib import safeget
+
+SUBTITLES = 'https://www.podnapisi.net'
+WDIR = os.getcwd()
+if not os.path.isdir('subs'): os.mkdir('subs')
+
+args = argparse.ArgumentParser(description='subtitles specs')
+args.add_argument('mname', metavar='movie name', type=str)
+args.add_argument('-lang', metavar='subtitle language', type=str, 
+                  nargs='+', default='en', choices=['en', 'sl'])
+vals = args.parse_args()
+
+# get subtitles
+url = SUBTITLES + '/en/subtitles/search/'
+query_page = safeget(url, params = {'keywords': vals.mname})
+assert query_page, 'Subtitles Response Not Valid'
+
+# filter results
+soup = BeautifulSoup(query_page.content, 'html.parser')
+subtitles = soup.select('table.table > tbody tr')
+for subtitle in subtitles:
+    lang = subtitle.select_one('td:nth-child(4) > a').attrs['href']
+    lang = parse_qs(urlsplit(lang).query)['language'][0]
+    if lang in vals.lang:
+        link = subtitle.select_one('div.pull-left > a').attrs['href']
+        file = safeget(SUBTITLES + link)
+        if not file: continue
+        ZipFile(BytesIO(file.content)).extractall(WDIR + '/subs')
+        if input('continue? y/n: ') == 'n': break
